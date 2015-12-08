1a0e08a3a964219b4290358c6cab4207661a6f21
piklasfam <piklases@gmail.com>
2015-12-08 09:10:51 -0500

    change

--- js/application.js
+++ js/application.js
@@ -1,35 +1,35 @@
 (function() {
-  'use strict';
-    var app = angular.module('app', ['app.controllers', 'app.directives', 'ui.router']);
-    app.config(function($stateProvider, $urlRouterProvider) {  
-	$stateProvider
-        .state('form', {
-            url: '/form',
-            templateUrl: 'pages/form.html',
-            controller: 'AppCtrl'
-        })
-        .state('form.step1', {
-            url: '/1',
-            templateUrl: 'pages/step1.html',
-        })
-        .state('form.step2', {
-            url: '/2',
-            templateUrl: 'pages/step2.html'
-        })
-        .state('form.step3', {
-            url: '/3',
-            templateUrl: 'pages/step3.html'
-        })
-        .state('form.step4', {
-            url: '/4',
-            templateUrl: 'pages/step4.html'
-        })
-        .state('form.total', {
-            url: '/total',
-            templateUrl: 'pages/total.html'
+        'use strict';
+        var app = angular.module('app', ['app.controllers', 'app.directives', 'ui.router']);
+        app.config(function($stateProvider, $urlRouterProvider) {  
+                $stateProvider
+                .state('form', {
+                        url: '/form',
+                        templateUrl: 'pages/form.html',
+                        controller: 'AppCtrl'
+                })
+                .state('form.step1', {
+                        url: '/1',
+                        templateUrl: 'pages/step1.html',
+                        controller: 'AppCtrl'
+                })
+                .state('form.step2', {
+                        url: '/2',
+                        templateUrl: 'pages/step2.html'
+                })
+                .state('form.step3', {
+                        url: '/3',
+                        templateUrl: 'pages/step3.html'
+                })
+                .state('form.step4', {
+                        url: '/4',
+                        templateUrl: 'pages/step4.html'
+                })
+                .state('form.total', {
+                        url: '/total',
+                        templateUrl: 'pages/total.html'
+                });
+                $urlRouterProvider.otherwise({redirectTo: "/form/1"});
         });
-    $urlRouterProvider.otherwise('/form/1');
-    
-    }
-  );
+        
 }).call(this);
